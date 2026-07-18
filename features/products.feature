fb788d25202ce2684d95be52eae672fc1a357d76
AyushGupta1712 <aayushg.g0024@gmail.com>
2026-07-18 01:39:56 -0400

    Complete TDD and BDD final project implementation

--- features/products.feature
+++ features/products.feature
@@ -21,7 +21,7 @@ Scenario: Create a Product
     And I set the "Name" to "Hammer"
     And I set the "Description" to "A claw hammer"
     And I select "True" in the "Available" dropdown
-    And I select "TOOLS" in the "Category" dropdown
+    And I select "Tools" in the "Category" dropdown
     And I set the "Price" to "34.95"
     And I press the "Create" button
     Then I should see the message "Success"
@@ -36,7 +36,7 @@ Scenario: Create a Product
     And I should see "Hammer" in the "Name" field
     And I should see "A claw hammer" in the "Description" field
     And I should see "True" in the "Available" dropdown
-    And I should see "TOOLS" in the "Category" dropdown
+    And I should see "Tools" in the "Category" dropdown
     And I should see "34.95" in the "Price" field
 
 Scenario: Read a Product
@@ -44,7 +44,7 @@ Scenario: Read a Product
     And I set the "Name" to "Hat"
     And I press the "Search" button
     Then I should see the message "Success"
-    And I should see "Hat" in the results
+    And I should see "A red fedora" in the "Description" field
     When I copy the "Id" field
     And I press the "Clear" button
     And I paste the "Id" field
@@ -53,7 +53,7 @@ Scenario: Read a Product
     And I should see "Hat" in the "Name" field
     And I should see "A red fedora" in the "Description" field
     And I should see "True" in the "Available" dropdown
-    And I should see "CLOTHS" in the "Category" dropdown
+    And I should see "Cloths" in the "Category" dropdown
     And I should see "59.95" in the "Price" field
 
 Scenario: Update a Product
@@ -61,25 +61,20 @@ Scenario: Update a Product
     And I set the "Name" to "Hat"
     And I press the "Search" button
     Then I should see the message "Success"
-    And I should see "Hat" in the results
-    When I copy the "Id" field
-    And I press the "Clear" button
-    And I paste the "Id" field
-    And I press the "Retrieve" button
-    Then I should see the message "Success"
-    And I should see "Hat" in the "Name" field
-    When I change the "Name" to "Fedora"
+    And I should see "A red fedora" in the "Description" field
+    When I change "Name" to "Fedora"
     And I press the "Update" button
     Then I should see the message "Success"
     When I copy the "Id" field
     And I press the "Clear" button
     And I paste the "Id" field
     And I press the "Retrieve" button
-    Then I should see "Fedora" in the "Name" field
+    Then I should see the message "Success"
+    And I should see "Fedora" in the "Name" field
     When I press the "Clear" button
-    And I set the "Name" to "Hat"
     And I press the "Search" button
     Then I should see the message "Success"
+    And I should see "Fedora" in the results
     And I should not see "Hat" in the results
 
 Scenario: Delete a Product
@@ -87,30 +82,41 @@ Scenario: Delete a Product
     And I set the "Name" to "Hat"
     And I press the "Search" button
     Then I should see the message "Success"
-    And I should see "Hat" in the results
+    And I should see "A red fedora" in the "Description" field
     When I copy the "Id" field
     And I press the "Clear" button
     And I paste the "Id" field
     And I press the "Delete" button
     Then I should see the message "Product has been Deleted!"
     When I press the "Clear" button
-    And I set the "Name" to "Hat"
     And I press the "Search" button
     Then I should see the message "Success"
     And I should not see "Hat" in the results
 
-Scenario: List by Category
+Scenario: List all products
     When I visit the "Home Page"
-    And I select "CLOTHS" in the "Category" dropdown
+    And I press the "Clear" button
     And I press the "Search" button
     Then I should see the message "Success"
     And I should see "Hat" in the results
     And I should see "Shoes" in the results
-    And I should not see "Big Mac" in the results
+    And I should see "Big Mac" in the results
+    And I should see "Sheets" in the results
+
+Scenario: Search by category
+    When I visit the "Home Page"
+    And I press the "Clear" button
+    And I select "Food" in the "Category" dropdown
+    And I press the "Search" button
+    Then I should see the message "Success"
+    And I should see "Big Mac" in the results
+    And I should not see "Hat" in the results
+    And I should not see "Shoes" in the results
     And I should not see "Sheets" in the results
 
-Scenario: List by Available
+Scenario: Search by available
     When I visit the "Home Page"
+    And I press the "Clear" button
     And I select "True" in the "Available" dropdown
     And I press the "Search" button
     Then I should see the message "Success"
@@ -119,12 +125,11 @@ Scenario: List by Available
     And I should see "Sheets" in the results
     And I should not see "Shoes" in the results
 
-Scenario: List by Name
+Scenario: Search by name
     When I visit the "Home Page"
-    And I set the "Name" to "Big Mac"
+    And I set the "Name" to "Hat"
     And I press the "Search" button
     Then I should see the message "Success"
-    And I should see "Big Mac" in the results
-    And I should not see "Hat" in the results
-    And I should not see "Shoes" in the results
-    And I should not see "Sheets" in the results
+    And I should see "Hat" in the "Name" field
+    And I should see "A red fedora" in the "Description" field
+    
--- features/steps/web_steps.py
+++ features/steps/web_steps.py
@@ -1,126 +1,89 @@
+######################################################################
+# Copyright 2016, 2023 John J. Rofrano. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+######################################################################
+"""
+Web Steps
+
+Steps file for web interactions with Selenium
+
+For information on Waiting until elements are present in the HTML see:
+    https://selenium-python.readthedocs.io/waits.html
+"""
+import logging
 from behave import when, then
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait, Select
+from selenium.webdriver.support.ui import Select, WebDriverWait
 from selenium.webdriver.support import expected_conditions
 
-# =================================================================
-# NAVIGATION & GENERAL STEPS
-# =================================================================
+ID_PREFIX = 'product_'
+
 
 @when('I visit the "Home Page"')
 def step_impl(context):
+    """ Make a call to the base URL """
     context.driver.get(context.base_url)
+    logging.info('Visiting %s', context.base_url)
 
-@then('I should see "{text}" in the title')
-def step_impl(context, text):
-    assert text in context.driver.title
 
-@then('I should not see "{text}"')
-def step_impl(context, text):
-    assert text not in context.driver.find_element(By.TAG_NAME, 'body').text
+@then('I should see "{message}" in the title')
+def step_impl(context, message):
+    """ Check the document title for a message """
+    assert(message in context.driver.title)
 
-# =================================================================
-# INTERACTIVE CONTROL STEPS (BUTTONS & DROPDOWNS)
-# =================================================================
+
+@then('I should not see "{text_string}"')
+def step_impl(context, text_string):
+    element = context.driver.find_element(By.TAG_NAME, 'body')
+    assert(text_string not in element.text)
+
+
+@when('I set the "{element_name}" to "{text_string}"')
+def step_impl(context, element_name, text_string):
+    element_id = ID_PREFIX + element_name.lower().replace(' ', '_')
+    element = context.driver.find_element(By.ID, element_id)
+    element.clear()
+    element.send_keys(text_string)
+
+
+@when('I change "{element_name}" to "{text_string}"')
+def step_impl(context, element_name, text_string):
+    element_id = ID_PREFIX + element_name.lower().replace(' ', '_')
+    element = context.driver.find_element(By.ID, element_id)
+    element.clear()
+    element.send_keys(text_string)
+
+
+@when('I select "{text_string}" in the "{element_name}" dropdown')
+def step_impl(context, text_string, element_name):
+    element_id = ID_PREFIX + element_name.lower().replace(' ', '_')
+    element = Select(context.driver.find_element(By.ID, element_id))
+    element.select_by_visible_text(text_string)
+
+
+@then('I should see "{text_string}" in the "{element_name}" dropdown')
+def step_impl(context, text_string, element_name):
+    element_id = ID_PREFIX + element_name.lower().replace(' ', '_')
+    element = Select(context.driver.find_element(By.ID, element_id))
+    assert(text_string == element.first_selected_option.text)
+
 
 @when('I press the "{button}" button')
 def step_impl(context, button):
     button_id = button.lower() + '-btn'
-    
-    # Force clear old results text on search to ensure clean asynchronously updated DOM polling
-    if button.lower() == 'search':
-        try:
-            context.driver.execute_script(
-                "document.getElementById('search_results').innerText = '';"
-            )
-        except Exception:
-            pass
-
     context.driver.find_element(By.ID, button_id).click()
 
-@when('I select "{value}" in the "{dropdown_name}" dropdown')
-def step_impl(context, value, dropdown_name):
-    name_lower = dropdown_name.lower()
-    element = None
-    
-    # Check for search form elements first using find_elements to avoid implicit wait penalties
-    for prefix in ['search_', 'product_']:
-        elements = context.driver.find_elements(By.ID, prefix + name_lower)
-        if elements and elements[0].is_displayed():
-            element = elements[0]
-            break
-            
-    if not element:
-        element = context.driver.find_element(By.ID, 'product_' + name_lower)
-
-    select = Select(element)
-    normalized_value = str(value).strip().lower()
-
-    # Dynamic option match strategy
-    target_text = None
-    for option in select.options:
-        opt_text = option.text.strip().lower()
-        opt_val = option.get_attribute('value').strip().lower()
-        if normalized_value == opt_text or normalized_value == opt_val:
-            target_text = option.text
-            break
-
-    if target_text is not None:
-        select.select_by_visible_text(target_text)
-    else:
-        select.select_by_value(value)
-
-    context.driver.execute_script("arguments[0].dispatchEvent(new Event('change'))", element)
-
-# =================================================================
-# TEXT DATA MUTATION & SETUP STEPS
-# =================================================================
-
-@when('I set the "{field_name}" to "{value}"')
-@when('I change the "{field_name}" to "{value}"')
-def step_impl(context, field_name, value):
-    element_id = 'product_' + field_name.lower()
-    element = context.driver.find_element(By.ID, element_id)
-    element.clear()
-    element.send_keys(value)
-
-# =================================================================
-# CLIPBOARD DATA STORAGE (COPY & PASTE OPERATIONS)
-# =================================================================
-
-@when('I copy the "{field_name}" field')
-def step_impl(context, field_name):
-    element_id = 'product_' + field_name.lower()
-    context.clipboard = context.driver.find_element(By.ID, element_id).get_attribute('value')
-
-@when('I paste the "{field_name}" field')
-def step_impl(context, field_name):
-    element_id = 'product_' + field_name.lower()
-    element = context.driver.find_element(By.ID, element_id)
-    element.clear()
-    element.send_keys(context.clipboard)
-
-# =================================================================
-# VALIDATION & VERIFICATION STEPS (THEN BLOCKS)
-# =================================================================
-
-@then('the "{field_name}" field should be empty')
-def step_impl(context, field_name):
-    element_id = 'product_' + field_name.lower()
-    field_value = context.driver.find_element(By.ID, element_id).get_attribute('value')
-    assert field_value == ""
-
-@then('I should see "{expected_value}" in the "{field_name}" field')
-def step_impl(context, expected_value, field_name):
-    element_id = 'product_' + field_name.lower()
-    actual_value = context.driver.find_element(By.ID, element_id).get_attribute('value')
-    assert actual_value == expected_value
-
-@then('I should see "{expected_value}" in the "{dropdown_name}" dropdown')
-def step_impl(context, expected_value, dropdown_name):
-    element_id = 'product_' + dropdown_name.lower()
-    select = Select(context.driver.find_element(By.ID, element_id))
-    assert select.first_selected_option.text.upper() == expected_value.upper()
 
 @then('I should see "{name}" in the results')
 def step_impl(context, name):
@@ -130,16 +93,14 @@ def step_impl(context, name):
             name
         )
     )
-    assert found
+    assert(found)
+
 
 @then('I should not see "{name}" in the results')
 def step_impl(context, name):
-    # Ensure any background active processing completes before validating absence
-    WebDriverWait(context.driver, context.wait_seconds).until(
-        expected_conditions.invisibility_of_element_located((By.ID, 'flash_message_loading'))
-    )
-    results_text = context.driver.find_element(By.ID, 'search_results').text
-    assert name not in results_text
+    element = context.driver.find_element(By.ID, 'search_results')
+    assert(name not in element.text)
+
 
 @then('I should see the message "{message}"')
 def step_impl(context, message):
@@ -149,4 +110,43 @@ def step_impl(context, message):
             message
         )
     )
-    assert found
+    assert(found)
+
+
+@when('I copy the "{element_name}" field')
+def step_impl(context, element_name):
+    element_id = ID_PREFIX + element_name.lower().replace(' ', '_')
+    element = WebDriverWait(context.driver, context.wait_seconds).until(
+        expected_conditions.presence_of_element_located((By.ID, element_id))
+    )
+    context.clipboard = element.get_attribute('value')
+    logging.info('Clipboard contains: %s', context.clipboard)
+
+
+@when('I paste the "{element_name}" field')
+def step_impl(context, element_name):
+    element_id = ID_PREFIX + element_name.lower().replace(' ', '_')
+    element = WebDriverWait(context.driver, context.wait_seconds).until(
+        expected_conditions.presence_of_element_located((By.ID, element_id))
+    )
+    element.clear()
+    element.send_keys(context.clipboard)
+
+
+@then('the "{element_name}" field should be empty')
+def step_impl(context, element_name):
+    element_id = ID_PREFIX + element_name.lower().replace(' ', '_')
+    element = context.driver.find_element(By.ID, element_id)
+    assert(element.get_attribute('value') == '')
+
+
+@then('I should see "{text_string}" in the "{element_name}" field')
+def step_impl(context, text_string, element_name):
+    element_id = ID_PREFIX + element_name.lower().replace(' ', '_')
+    found = WebDriverWait(context.driver, context.wait_seconds).until(
+        expected_conditions.text_to_be_present_in_element_value(
+            (By.ID, element_id),
+            text_string
+        )
+    )
+    assert(found)
